014b387b2413ab450fc4be7c283016b12cb5692e
fernandoformanek <fernandograssi4@gmail.com>
2025-08-13 11:00:18 -0300

    Exercicio 1

---
+++ exercicios.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char nome[50];
+    int idade;
+    char curso[50];
+    printf("Digite seu nome ");
+    scanf("%s", nome);
+    printf("Digite sua idade ");
+    scanf("%d", &idade);
+    printf("Digite seu curso ");
+    scanf("%s", curso);
+
+    int ano_nascimento = 2025 - idade;
+    printf("\nOla %s!\n Seja bem-vindo ao curso de %s, voce tem aproximadamente %d anos.", nome, curso, idade);
+    return 0;
+}
